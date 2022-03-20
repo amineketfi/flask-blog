0fc9e14291049eb5c8300edc3c06c19cc16cf074
amineketfi <amineketfi.cs50x@gmail.com>
2022-03-20 17:42:41 +0100

    adding login form to registartion system plus using url_for in the navbar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash
 from forms import LoginForm, RegistrationFrom
 app = Flask(__name__)
 
@@ -33,15 +33,25 @@ def about():
     return render_template('about.html', title="About")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+        
     return render_template('login.html', title='Login', form=form)
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationFrom()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
 
 

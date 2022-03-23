fd7375fcbc39618d83315e9d20c4afc0e8799fc1
amineketfi <amineketfi.cs50x@gmail.com>
2022-03-23 12:05:24 +0100

    Update User Athentification System

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationFrom(FlaskForm):
     username = StringField('Username',
@@ -9,10 +10,21 @@ class RegistrationFrom(FlaskForm):
                              validators=[DataRequired(), Email()])
     password = PasswordField('Password',
                              validators=[DataRequired(), Length(min=8)])
-    password_confirm = PasswordField('Password',
+    password_confirm = PasswordField('Confirm Password',
                              validators=[DataRequired(), Length(min=8), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username is taken. Please choose a different one')
 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email is taken. Please choose a different one')
+
+    
     
 class LoginForm(FlaskForm):
     email = StringField('Email',

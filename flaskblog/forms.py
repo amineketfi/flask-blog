edba11dd9dc2252228da9f472b579c958f174e67
amineketfi <amineketfi.cs50x@gmail.com>
2022-03-28 13:33:03 +0100

    Add upload profile image functionality and update account

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,7 @@
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -33,3 +36,26 @@ class LoginForm(FlaskForm):
                          validators=[DataRequired(), Length(min=8)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
+
+
+
+class UpdateAccountFrom(FlaskForm):
+    username = StringField('Username',
+                          validators=[DataRequired(), Length(min=6, max=16)])
+    email = StringField('Email',
+                             validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('username is taken. Please choose a different one')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('email is taken. Please choose a different one')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,32 +1,34 @@
 
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, redirect, flash, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import LoginForm, RegistrationFrom
+from flaskblog.forms import LoginForm, RegistrationFrom, UpdateAccountFrom
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
 posts = [
     {
-        'auther' : 'Ilyes Sellam',
-        'title' : 'Blog Post 1',
-        'content' : 'This is my first flask app',
+        'auther': 'Ilyes Sellam',
+        'title': 'Blog Post 1',
+        'content': 'This is my first flask app',
         'date_posted': 'April 20, 2022'
     },
     {
-        'auther' : 'Amine Ketfi',
-        'title' : 'Blog Post 2',
-        'content' : 'This is my first flask app me too',
+        'auther': 'Amine Ketfi',
+        'title': 'Blog Post 2',
+        'content': 'This is my first flask app me too',
         'date_posted': 'April 21, 2022'
     }
 ]
 
 
-
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template('index.html', posts = posts, title="Home")
+    return render_template('index.html', posts=posts, title="Home")
 
 
 @app.route("/about")
@@ -38,7 +40,7 @@ def about():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -48,7 +50,7 @@ def login():
             return redirect(next_page) if next_page else redirect(url_for('index'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-        
+
     return render_template('login.html', title='Login', form=form)
 
 
@@ -56,11 +58,13 @@ def login():
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-        
+
     form = RegistrationFrom()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account has been created! You are now able to log in', 'success')
@@ -73,7 +77,36 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)    
+    i.save(picture_path)
+    
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountFrom()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file= save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                            image_file=image_file, form=form)

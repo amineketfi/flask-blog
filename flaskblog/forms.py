bf018c39dc9f42b2e05a67ebac8cf3a9d5d02149
amineketfi <amineketfi.cs50x@gmail.com>
2022-03-29 15:36:03 +0100

    adding posts ..

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -2,7 +2,7 @@ from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -59,3 +59,9 @@ class UpdateAccountFrom(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('email is taken. Please choose a different one')
+            
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,32 +2,17 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, redirect, flash, request
+from flask import render_template, url_for, redirect, flash, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import LoginForm, RegistrationFrom, UpdateAccountFrom
+from flaskblog.forms import LoginForm, RegistrationFrom, UpdateAccountFrom, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-
-posts = [
-    {
-        'auther': 'Ilyes Sellam',
-        'title': 'Blog Post 1',
-        'content': 'This is my first flask app',
-        'date_posted': 'April 20, 2022'
-    },
-    {
-        'auther': 'Amine Ketfi',
-        'title': 'Blog Post 2',
-        'content': 'This is my first flask app me too',
-        'date_posted': 'April 21, 2022'
-    }
-]
 
 
 @app.route("/")
 @app.route("/home")
 def index():
+    posts = Post.query.all()
     return render_template('index.html', posts=posts, title="Home")
 
 
@@ -110,3 +95,55 @@ def account():
         'static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                             image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('index'))
+    return render_template('create_post.html', title='New Post',
+                           form = form, legend='New Post')
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form = form, legend='Update Post')
+
+
+    
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('index'))
